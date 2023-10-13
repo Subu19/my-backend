35b181707ea13152f6b66e71b1ec5a0f142108fb
Subu <59548115+Subu19@users.noreply.github.com>
2023-10-13 18:30:08 +0545

    update security

--- config/middlewares.js
+++ config/middlewares.js
@@ -6,7 +6,7 @@ module.exports = [
       contentSecurityPolicy: {
         useDefaults: true,
         directives: {
-          "connect-src": ["'self'", "http:", "https:"],
+          "connect-src": ["'self'", "http:", "https:", "img-src"],
           upgradeInsecureRequests: null,
         },
       },
